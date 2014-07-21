0d0259b3fc29ac27331b79dc4d34635582119902
Darren Reid <layoric@gmail.com>
2014-07-21 13:26:30 +1000

    Renamed project. Updated template names. Updated Razor project structure and deps.

---
+++ src/VSServiceStack/ProjectTemplates/ServiceStack Razor Seed/ServiceInterface/MyServices.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ServiceStack;
+using ServiceStack.CSharp.RazorSeed.ServiceModel;
+
+namespace $safeprojectname$
+{
+    public class MyServices : Service
+    {
+        public object Any(Hello request)
+        {
+            return new HelloResponse { Result = "Hello, {0}".Fmt(request.Name) };
+        }
+    }
+}
