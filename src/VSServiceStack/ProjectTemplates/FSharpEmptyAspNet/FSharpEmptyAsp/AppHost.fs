fab293c3be13d056a454aa0cc770ac0a5cf16ad5
Darren Reid <layoric@gmail.com>
2014-08-20 19:30:11 +1000

    Added FSharp Empty ASPNET template

---
+++ src/VSServiceStack/ProjectTemplates/FSharpEmptyAspNet/FSharpEmptyAsp/AppHost.fs
@@ -0,0 +1,20 @@
+﻿namespace $safeprojectname$
+    open ServiceStack
+    open ServiceStack.Common
+    open $safeprojectname$
+    open $safeprojectname$.ServiceInterface
+    open System
+
+    type AppHost() = 
+        inherit AppHostBase("$safeprojectname$", typeof<MyServices>.Assembly)
+        override this.Configure container =
+            //Add plugins
+            //this.Plugins.Add(new PostmanFeature())
+            //this.Plugins.Add(new CorsFeature())
+            ignore()
+        static member Start() = 
+            let apphost = new AppHost()
+            apphost.Init() |> ignore
+            
+            
+
---
+++ src/VSServiceStack/ProjectTemplates/FSharpEmptyAspNet/ServiceInterface/MyServices.fs
@@ -0,0 +1,8 @@
+﻿namespace $safeprojectname$
+
+open ServiceStack
+open $saferootprojectname$.ServiceModel
+
+type MyServices() = 
+    inherit Service()
+    member this.Any(request : Hello) = { Result = "Hello, {0}!".Fmt(request.Name) }
