e1bddfd6478783fbb812a0b5532ad4bace848d68
Darren Reid <layoric@gmail.com>
2014-11-07 16:07:13 +1100

    WIP Auto packaging updating.

--- src/ServiceStackVS.Wizards/NuGetPackageInstallerWizard.cs
+++ src/ServiceStackVS.Wizards/NuGetPackageInstallerWizard.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.ComponentModel.Composition.Hosting;
+using System.IO;
 using System.Linq;
 using System.Security.Principal;
 using System.Text;
@@ -12,6 +13,7 @@ using EnvDTE;
 using Microsoft.VisualStudio.ComponentModelHost;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.TemplateWizard;
+using NuGet;
 using NuGet.VisualStudio;
 using ServiceStack;
 using IServiceProvider = Microsoft.VisualStudio.OLE.Interop.IServiceProvider;
@@ -33,15 +35,13 @@ namespace ServiceStackVS.Wizards
 
         private List<PackageFromWizard> PackagesToLoad = new List<PackageFromWizard>();
 
-        private bool NpmInstalled;
-
         public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
         {
             if (runKind == WizardRunKind.AsNewProject)
             {
-                using (var serviceProvider = new ServiceProvider((IServiceProvider)automationObject))
+                using (var serviceProvider = new ServiceProvider((IServiceProvider) automationObject))
                 {
-                    var componentModel = (IComponentModel)serviceProvider.GetService(typeof(SComponentModel));
+                    var componentModel = (IComponentModel) serviceProvider.GetService(typeof (SComponentModel));
                     using (var container = new CompositionContainer(componentModel.DefaultExportProvider))
                     {
                         container.ComposeParts(this);
@@ -50,46 +50,20 @@ namespace ServiceStackVS.Wizards
 
                 string wizardData = replacementsDictionary["$wizarddata$"];
                 XElement element = XElement.Parse(wizardData);
-                bool promptUser = element.HasAttributes && element.Attribute("promptUser") != null &&
-                                  element.Attribute("promptUser").Value.EqualsIgnoreCase("true");
-                if (promptUser)
-                {
-                    var dialogResult = MessageBox.Show("Do you want to install Node.JS and related dependencies locally?", "Node.JS Depedencies", MessageBoxButtons.YesNo);
-                    if (dialogResult == System.Windows.Forms.DialogResult.Yes)
-                    {
-                        NpmInstalled = true;
-                        var user = WindowsIdentity.GetCurrent();
-                        if (user == null)
-                        {
-                            throw new Exception("Error creating template, no valid user identity");
-                        }
-                        // Get the built-in administrator account.
-                        var sid = new SecurityIdentifier(WellKnownSidType.BuiltinAdministratorsSid,
-                            null);
 
-                        // Compare to the current user.
-                        bool isBuiltInAdmin = (user.User == sid);
-                        if (!isBuiltInAdmin)
-                        {
-                            MessageBox.Show(
-                                "Warning: This template requires the ability to set VS properties, without the correct permissions these will not persist. Run as Administrator to ensure these settings persist.",
-                                "Template requires administrator rights", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                        }
-                        var packages =
-                            element.Descendants()
-                                .Select(
-                                    x =>
-                                        new PackageFromWizard
-                                        {
-                                            Id = x.Attribute("id").Value,
-                                            Version = x.Attribute("version").Value
-                                        });
+                var packages =
+                    element.Descendants()
+                        .Select(
+                            x =>
+                                new PackageFromWizard
+                                {
+                                    Id = x.Attribute("id").Value,
+                                    Version = x.Attribute("version").Value
+                                });
 
-                        PackagesToLoad = new List<PackageFromWizard>(packages);
-                    }
-                }
-                
+                PackagesToLoad = new List<PackageFromWizard>(packages);
             }
+
         }
 
         public void ProjectFinishedGenerating(Project project)
@@ -123,6 +97,40 @@ namespace ServiceStackVS.Wizards
         private void AddNuGetDependencyIfMissing(Project project, string packageId, string version = null)
         {
             var installedPackages = PackageServices.GetInstalledPackages(project);
+            
+            IPackageRepository nugetv2Service = PackageRepositoryFactory.Default.CreateRepository("https://packages.nuget.org/api/v2");
+            string userAppData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string cachePath = Path.Combine(userAppData, "..\\Local\\NuGet\\Cache");
+            bool cacheExists = Directory.Exists(cachePath);
+            if (cacheExists)
+            {
+                IPackageRepository localCache = PackageRepositoryFactory.Default.CreateRepository(cachePath);
+
+                //Try use cache
+                try
+                {
+                    List<IPackage> latestNugetPackages = nugetv2Service.FindPackagesById(packageId).ToList();
+                    var latestNugetPackage = latestNugetPackages.FirstOrDefault(x => x.IsLatestVersion);
+                    List<IPackage> latestCachePackages = nugetv2Service.FindPackagesById(packageId).ToList();
+                    var latestCachePackage = latestCachePackages.FirstOrDefault(x => x.IsLatestVersion);
+                    bool useCache = latestNugetPackage.Version == latestCachePackage.Version;
+                    if (useCache)
+                    {
+                        Installer.InstallPackage(
+                            cachePath, 
+                            project, 
+                            packageId,
+                            version: latestCachePackage.Version.ToString(), //Null is latest version of packageId
+                            ignoreDependencies: false);
+                    }
+                }
+                catch (Exception)
+                {
+                    //Something went wrong with cache, fall back to NuGet
+
+                }
+            }
+            
             version = string.IsNullOrEmpty(version) || version == "latest" ? null : version; //if empty or latest, set to null
             //Check if existing nuget reference exists
             if (installedPackages.FirstOrDefault(x => x.Id == packageId) == null)
