32790e1ae269405792708ecad416c591284a3ebe
Darren Reid <layoric@gmail.com>
2014-11-27 16:37:38 +1100

    Default example tests for VSIX project template don't work... Removed and added some tests for native types behaviour. Sticking with MS Tests due to requirement that library needs to be signed. Will switch if NUnit is signed.

---
+++ src/ServiceStackVS/VSServiceStack_UnitTests/DocumentExtensionTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnvDTE;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServiceStackVS.FileHandlers;
+using ServiceStackVS;
+
+namespace ServiceStackVS_UnitTests
+{
+    [TestClass]
+    public class DocumentExtensionTests
+    {
+        [TestMethod]
+        public void Get_typehandler_from_file_list()
+        {
+            Dictionary<string, Type> selectedItems = new Dictionary<string, Type>
+            {
+                {"foo.dto.fs",typeof (FSharpNativeTypesHandler)},
+                {"foo.dtos.fs",typeof (FSharpNativeTypesHandler)},
+                {"foo.dtos.cs",typeof (CSharpNativeTypesHandler)},
+                {"foo.dtos.vb",typeof (VbNetNativeTypesHandler)},
+                {"foo.dtos.d.ts",typeof (TypeScriptNativeTypesHandler)},
+            };
+
+            foreach (var selectedItem in selectedItems)
+            {
+                var listOfItems = new List<SelectedItem>();
+                listOfItems.Add(new MockSelecedItem { Name = selectedItem.Key });
+                var typeOfHandler = listOfItems.GetTypeHandlerForSelectedFiles().First().GetType();
+                Assert.AreEqual(selectedItem.Value, typeOfHandler);
+            }
+        }
+    }
+
+    public class MockSelecedItem : SelectedItem
+    {
+        public string Name { get; set; }
+
+        //This here due to interface
+        public SelectedItems Collection { get; private set; }
+        public DTE DTE { get; private set; }
+        public Project Project { get; private set; }
+        public ProjectItem ProjectItem { get; private set; }
+        
+        public short InfoCount { get; private set; }
+        public object get_Info(short index)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/ServiceStackVS/VSServiceStack_UnitTests/NativeTypeUrlTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServiceStackVS.FileHandlers;
+
+namespace ServiceStackVS_UnitTests
+{
+    [TestClass]
+    public class NativeTypeUrlTests
+    {
+        [TestMethod]
+        public void CSharp_builds_url()
+        {
+            var typeHandler = new CSharpNativeTypesHandler();
+            Dictionary<string, string> validUrls = new Dictionary<string,string>
+            {
+                {"http://localhost:8080/","http://localhost:8080/types/csharp"},
+                {"http://example.com","http://example.com/types/csharp"},
+                {"https://localhost:81/apis","https://localhost:81/apis/types/csharp"},
+                {"https://localhost","https://localhost/types/csharp"}
+            };
+
+            foreach (var validUrl in validUrls)
+            {
+                Assert.AreEqual(validUrl.Value, typeHandler.GenerateUrl(validUrl.Key,null));
+            }
+        }
+
+        [TestMethod]
+        public void FSharp_builds_url()
+        {
+            var typeHandler = new FSharpNativeTypesHandler();
+            Dictionary<string, string> validUrls = new Dictionary<string, string>
+            {
+                {"http://localhost:8080/","http://localhost:8080/types/fsharp"},
+                {"http://example.com","http://example.com/types/fsharp"},
+                {"https://localhost:81/apis","https://localhost:81/apis/types/fsharp"},
+                {"https://localhost","https://localhost/types/fsharp"}
+            };
+
+            foreach (var validUrl in validUrls)
+            {
+                Assert.AreEqual(validUrl.Value, typeHandler.GenerateUrl(validUrl.Key, null));
+            }
+        }
+
+        [TestMethod]
+        public void VbNet_builds_url()
+        {
+            var typeHandler = new VbNetNativeTypesHandler();
+            Dictionary<string, string> validUrls = new Dictionary<string, string>
+            {
+                {"http://localhost:8080/","http://localhost:8080/types/vbnet"},
+                {"http://example.com","http://example.com/types/vbnet"},
+                {"https://localhost:81/apis","https://localhost:81/apis/types/vbnet"},
+                {"https://localhost","https://localhost/types/vbnet"}
+            };
+
+            foreach (var validUrl in validUrls)
+            {
+                Assert.AreEqual(validUrl.Value, typeHandler.GenerateUrl(validUrl.Key, null));
+            }
+        }
+
+        [TestMethod]
+        public void TypeScript_builds_url()
+        {
+            var typeHandler = new TypeScriptNativeTypesHandler();
+            Dictionary<string, string> validUrls = new Dictionary<string, string>
+            {
+                {"http://localhost:8080/","http://localhost:8080/types/typescript.d"},
+                {"http://example.com","http://example.com/types/typescript.d"},
+                {"https://localhost:81/apis","https://localhost:81/apis/types/typescript.d"},
+                {"https://localhost","https://localhost/types/typescript.d"}
+            };
+
+            foreach (var validUrl in validUrls)
+            {
+                Assert.AreEqual(validUrl.Value, typeHandler.GenerateUrl(validUrl.Key, null));
+            }
+        }
+    }
+}
