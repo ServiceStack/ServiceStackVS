fe441923ef1e40815df2c92c0a2f98b6fc49b453
Darren Reid <layoric@gmail.com>
2015-01-21 12:45:11 +1100

    Added Self host Razor template for SSVS.

---
+++ src/ServiceStackVS/ProjectTemplates/RazorSelfHost/RazorSelfHost/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceStack.Text;
+
+namespace $safeprojectname$
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new AppHost().Init().Start("http://*:8088/");
+            "ServiceStack Self Host with Razor listening at http://localhost:8088 ".Print();
+            Process.Start("http://localhost:8088/");
+
+            Console.ReadLine();
+        }
+    }
+}
