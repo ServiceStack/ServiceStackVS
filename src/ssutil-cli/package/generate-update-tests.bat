a0ed2bf284a6e59f83d81422be3a7fabb00023d7
Darren Reid <darren.j.reid@gmail.com>
2015-06-14 17:51:54 +1000

    Rename build dir to package

---
+++ src/ssutil-cli/package/generate-update-tests.bat
@@ -0,0 +1,6 @@
+ssutil-cli "http://techstacks.io/" --file ..\..\ssutil-cli.Tests\TestDtos\ServiceStackRef_Update --lang CSharp
+ssutil-cli "http://techstacks.io/" --file ..\..\ssutil-cli.Tests\TestDtos\ServiceStackRef_Update --lang FSharp
+ssutil-cli "http://techstacks.io/" --file ..\..\ssutil-cli.Tests\TestDtos\ServiceStackRef_Update --lang Typescript.d
+ssutil-cli "http://techstacks.io/" --file ..\..\ssutil-cli.Tests\TestDtos\ServiceStackRef_Update --lang VBNet
+ssutil-cli "http://techstacks.io/" --file ..\..\ssutil-cli.Tests\TestDtos\ServiceStackRef_Update --lang Java
+ssutil-cli "http://techstacks.io/" --file ..\..\ssutil-cli.Tests\TestDtos\ServiceStackRef_Update --lang Swift
