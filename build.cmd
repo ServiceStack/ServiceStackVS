f82aac4198cb8f8acd6a4f9996479c37cb1d3647
Darren Reid <darren.j.reid@gmail.com>
2015-06-22 08:00:04 +1000

    Fix for #11 and to build script

--- build.cmd
+++ build.cmd
@@ -2,4 +2,4 @@ set VSToolsPath=C:\Program Files (x86)\MSBuild\Microsoft\VisualStudio\v12.0
 
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild .\src\ServiceStackVS\ServiceStackVS.csproj /t:Build /p:Configuration=Release /p:TargetFramework=v4.5 /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 
-xcopy "src\ServiceStackVS\\bin\Release\ServiceStackVS.vsix" "dist"
+xcopy "src\ServiceStackVS\\bin\Release\ServiceStackVS.vsix" "dist" /b/v/y
