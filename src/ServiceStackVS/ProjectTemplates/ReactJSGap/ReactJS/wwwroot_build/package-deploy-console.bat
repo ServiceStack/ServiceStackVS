aa9e4b6871a0ab960f97380769a72ae191e07817
Darren Reid <darren.j.reid@gmail.com>
2015-08-02 11:48:41 +1000

    Clean up console output on first run

--- src/ServiceStackVS/ProjectTemplates/ReactJSGap/ReactJS/wwwroot_build/package-deploy-console.bat
+++ src/ServiceStackVS/ProjectTemplates/ReactJSGap/ReactJS/wwwroot_build/package-deploy-console.bat
@@ -23,9 +23,13 @@ SET INPUT=%INPUT% %RELEASE%\System.Web.Razor.dll
 
 %ILMERGE% /target:exe /targetplatform:v4,"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5" /out:staging-console\%OUTPUTNAME% /ndebug %INPUT% 
 
+IF EXIST $safeprojectname$-console.7z (
 del $safeprojectname$-console.7z
+)
+
+IF EXIST $safeprojectname$-console.exe (
 del $safeprojectname$-console.exe
+)
 
 cd tools && 7za a ..\$safeprojectname$-console.7z ..\staging-console\* && cd..
-
 copy /b .\tools\7zsd_All.sfx + config-console.txt + $safeprojectname$-console.7z $safeprojectname$-console.exe
--- src/ServiceStackVS/ProjectTemplates/ReactJSGap/ReactJS/wwwroot_build/package-deploy-winforms.bat
+++ src/ServiceStackVS/ProjectTemplates/ReactJSGap/ReactJS/wwwroot_build/package-deploy-winforms.bat
@@ -10,9 +10,13 @@ COPY %RELEASE%\$safeprojectname$.AppWinForms.exe .\staging-winforms
 COPY %RELEASE%\CefSharp.BrowserSubprocess.exe .\staging-winforms
 ROBOCOPY "%RELEASE%" ".\staging-winforms" *.dll *.pak *.dat /E
 
+IF EXIST $safeprojectname$-winforms.7z (
 del $safeprojectname$-winforms.7z
+)
+
+IF EXIST $safeprojectname$-winforms.exe (
 del $safeprojectname$-winforms.exe
+)
 
 cd tools && 7za a ..\$safeprojectname$-winforms.7z ..\staging-winforms\* && cd..
-
 copy /b .\tools\7zsd_All.sfx + config-winforms.txt + $safeprojectname$-winforms.7z $safeprojectname$-winforms.exe
