ae2755dd89218f48caf22d60f135e8965521b7dc
Darren Reid <darren.j.reid@gmail.com>
2015-08-21 10:09:34 +1000

    Added missing dlls for ILMerge and React Desktop Apps template

--- src/ServiceStackVS/ProjectTemplates/ReactJSGap/ReactJS/wwwroot_build/package-deploy-console.bat
+++ src/ServiceStackVS/ProjectTemplates/ReactJSGap/ReactJS/wwwroot_build/package-deploy-console.bat
@@ -17,7 +17,9 @@ SET INPUT=%INPUT% %RELEASE%\ServiceStack.Text.dll
 SET INPUT=%INPUT% %RELEASE%\ServiceStack.Client.dll
 SET INPUT=%INPUT% %RELEASE%\ServiceStack.Common.dll
 SET INPUT=%INPUT% %RELEASE%\ServiceStack.Interfaces.dll
+SET INPUT=%INPUT% %RELEASE%\ServiceStack.Server.dll
 SET INPUT=%INPUT% %RELEASE%\ServiceStack.OrmLite.dll
+SET INPUT=%INPUT% %RELEASE%\ServiceStack.Redis.dll
 SET INPUT=%INPUT% %RELEASE%\ServiceStack.Razor.dll
 SET INPUT=%INPUT% %RELEASE%\System.Web.Razor.dll
 
