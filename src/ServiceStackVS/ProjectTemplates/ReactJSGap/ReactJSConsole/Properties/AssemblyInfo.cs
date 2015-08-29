80c0f70ebe87fb1addc9cb21d5174a54eb4bbacd
Darren Reid <darren.j.reid@gmail.com>
2015-08-29 16:13:32 +1000

    Remove Mac template from SSVS
    Updated assembly info.
    Replace SFX output with better SS logo.

--- src/ServiceStackVS/ProjectTemplates/ReactJSGap/ReactJSConsole/Properties/AssemblyInfo.cs
+++ src/ServiceStackVS/ProjectTemplates/ReactJSGap/ReactJSConsole/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("$safeprojectname$")]
+[assembly: AssemblyTitle("$saferootprojectname$")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("$safeprojectname$")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
+[assembly: AssemblyCompany("ServiceStack")]
+[assembly: AssemblyProduct("$saferootprojectname$")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- src/ServiceStackVS/ProjectTemplates/ReactJSGap/ReactJSWinForms/Properties/AssemblyInfo.cs
+++ src/ServiceStackVS/ProjectTemplates/ReactJSGap/ReactJSWinForms/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("$safeprojectname$")]
+[assembly: AssemblyTitle("$saferootprojectname$")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("$safeprojectname$")]
+[assembly: AssemblyCompany("ServiceStack")]
+[assembly: AssemblyProduct("$saferootprojectname$")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
