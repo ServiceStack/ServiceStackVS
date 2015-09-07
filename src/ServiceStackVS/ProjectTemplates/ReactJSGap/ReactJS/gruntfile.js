92ea206e1338db6e4358f81d018c7be044eccf76
Darren Reid <darren.j.reid@gmail.com>
2015-09-08 07:46:20 +1000

    Add auto generated header for default.cshtml in Resources.

--- src/ServiceStackVS/ProjectTemplates/ReactJSGap/ReactJS/gruntfile.js
+++ src/ServiceStackVS/ProjectTemplates/ReactJSGap/ReactJS/gruntfile.js
@@ -208,6 +208,7 @@ module.exports = function (grunt) {
                     .pipe(gulpif('*.css', minifyCss()))
                     .pipe(assets.restore())
                     .pipe(useref())
+                    .pipe(header("@*Auto generated file on " + (new Date().toLocaleString()) + ". See $safeprojectname$ project grunt file*@\r\n"))
                     .pipe(gulp.dest(resourcesRoot))
                     .pipe(gulp.dest(webRoot));
 
