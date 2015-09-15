c0e9ed84668bebd15803c80abc177fc3713a7efe
Demis Bellot <demis.bellot@gmail.com>
2015-09-14 23:27:14 -0700

    Default background color to white to avoid ugly grey patch whilst maximising

--- src/ServiceStackVS/ProjectTemplates/ReactJSGap/ReactJSWinForms/FormMain.Designer.cs
+++ src/ServiceStackVS/ProjectTemplates/ReactJSGap/ReactJSWinForms/FormMain.Designer.cs
@@ -35,6 +35,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(284, 262);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FormMain";
