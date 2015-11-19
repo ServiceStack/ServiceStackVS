65473540e4b246d8d90e6c9ed51579b72a67736f
Darren Reid <darren.j.reid@gmail.com>
2015-11-19 15:30:22 +1100

    Revert "More fixes for SSXS"
    This reverts commit 2f46ad2c5b5175b4767899f7095680b0eeec0d7e.

---
+++ src/ServiceStackXS/Properties/AddinInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: Addin("ServiceStackXS",
+    Namespace = "ServiceStackXS",
+	Version = "0.2",
+	Category = "IDE extensions")]
+
+[assembly: AddinName("ServiceStackXS")]
+[assembly: AddinDescription("An extension that enhances the development experience when working with the ServiceStack framework.")]
+
+[assembly: AddinDependency ("::MonoDevelop.Core", MonoDevelop.BuildInfo.CompatVersion)]
+[assembly: AddinDependency("::MonoDevelop.Ide", MonoDevelop.BuildInfo.CompatVersion)]
