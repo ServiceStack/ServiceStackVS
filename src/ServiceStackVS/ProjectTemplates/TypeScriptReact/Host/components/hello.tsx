0a9b75797c6e797c2abb8c6bf006e55601438b32
Darren Reid <darren.j.reid@gmail.com>
2016-02-20 17:15:06 +1100

    Min hello component calling service + cleanup

--- src/ServiceStackVS/ProjectTemplates/TypeScriptReact/Host/components/hello.tsx
+++ src/ServiceStackVS/ProjectTemplates/TypeScriptReact/Host/components/hello.tsx
@@ -1,12 +1,39 @@
 ﻿// A '.tsx' file enables JSX support in the TypeScript compiler, 
 // for more information see the following page on the TypeScript wiki:
 // https://github.com/Microsoft/TypeScript/wiki/JSX
-/// <reference path="../typings/browser.d.ts" />
+/// <reference path='../typings/browser/ambient/jquery/jquery.d.ts'/>
 
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import 'jquery';
 export default class HelloWorld extends React.Component<any, any> {
+    constructor(props, context) {
+        super(props, context);
+        this.state = { yourName: '' };
+    }
+
+    update(event: any) {
+        event.preventDefault();
+        var yourName = event.target.value;
+        var self = this;
+        $.ajax({
+            url: 'hello/' + yourName,
+            dataType: 'json',
+            type: 'GET',
+            success(response) {
+                self.setState({ yourName: response.Result });
+            }
+        });
+    }
+
+    handleChange(event:any) {
+        this.setState({ yourName: event.target.value });
+    }
+
     render() {
-        return <div>Hello, World!!!</div>;
+        return <div>
+                <input type="text" value={this.state.value} onChange={this.update.bind(this)} />
+                <span>{this.state.yourName}</span>
+            </div>;
     }
 }
--- src/ServiceStackVS/ProjectTemplates/TypeScriptReact/Host/wwwroot_build/build.bat
+++ src/ServiceStackVS/ProjectTemplates/TypeScriptReact/Host/wwwroot_build/build.bat
@@ -1,11 +1,3 @@
-echo "Starting initial build..."
-
 call jspm install
 
 call typings install
-
-msbuild "..\..\$safeprojectname$.sln" /p:configuration=debug
-
-call jspm bundle deps deps.lib.js
-
-echo "Finished initial build."
