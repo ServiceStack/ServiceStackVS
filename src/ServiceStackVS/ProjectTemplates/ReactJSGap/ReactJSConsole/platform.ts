17dd4b23d55d78f737c80124811052d2127c85c8
Darren Reid <darren.j.reid@gmail.com>
2016-03-14 14:47:19 +1100

    Fix pathing

--- src/ServiceStackVS/ProjectTemplates/ReactJSGap/ReactJSConsole/platform.ts
+++ src/ServiceStackVS/ProjectTemplates/ReactJSGap/ReactJSConsole/platform.ts
@@ -1,9 +1,9 @@
 ﻿// A '.tsx' file enables JSX support in the TypeScript compiler, 
 // for more information see the following page on the TypeScript wiki:
 // https://github.com/Microsoft/TypeScript/wiki/JSX
-/// <reference path="../$safeprojectname$/platform.d.ts" />
-/// <reference path='../$safeprojectname$/typings/browser.d.ts'/>
-import Platform = $safeprojectname$.Platform;
+/// <reference path="../$saferootprojectname$/platform.d.ts" />
+/// <reference path='../$saferootprojectname$/typings/browser.d.ts'/>
+import Platform = $saferootprojectname$.Platform;
 
 class ConsoleNativeHost implements Platform.INativeHost {
     showAbout() {
--- src/ServiceStackVS/ProjectTemplates/ReactJSGap/ReactJSWinForms/platform.ts
+++ src/ServiceStackVS/ProjectTemplates/ReactJSGap/ReactJSWinForms/platform.ts
@@ -1,8 +1,8 @@
 ﻿// A '.tsx' file enables JSX support in the TypeScript compiler, 
 // for more information see the following page on the TypeScript wiki:
 // https://github.com/Microsoft/TypeScript/wiki/JSX
-/// <reference path='../$safeprojectname$/typings/browser.d.ts'/>
-/// <reference path="../$safeprojectname$/platform.d.ts" />
+/// <reference path='../$saferootprojectname$/typings/browser.d.ts'/>
+/// <reference path="../$saferootprojectname$/platform.d.ts" />
 document.documentElement.className += ' winforms';
 $(document).ready(() => {
     window['nativeHost'].ready();
