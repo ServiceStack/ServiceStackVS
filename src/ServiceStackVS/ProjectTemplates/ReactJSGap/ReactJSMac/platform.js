18c0f6bb4e67c8ee2b22c413895dc2ee57807867
Darren Reid <darren.j.reid@gmail.com>
2016-03-22 07:55:58 +1100

    Default platform.js for mac proj

--- src/ServiceStackVS/ProjectTemplates/ReactJSGap/ReactJSMac/platform.js
+++ src/ServiceStackVS/ProjectTemplates/ReactJSGap/ReactJSMac/platform.js
@@ -1,14 +1,15 @@
-/* mac */
-document.documentElement.className += ' mac';
-window.nativeHost = {
-    quit: function () {
-        $.post('/nativehost/quit');
-    },
-    showAbout: function () {
-    	$.post('/nativehost/showAbout');
-    },
-    ready: function () {
-        //
-    },
-    platform: 'mac'
-};
+var MacNativeHost = (function () {
+    function MacNativeHost() {
+    }
+    MacNativeHost.prototype.showAbout = function () {
+        alert("ReactDesktopTest - ServiceStack + ReactJS");
+    };
+    MacNativeHost.prototype.toggleFormBorder = function () { };
+    MacNativeHost.prototype.quit = function () {
+        window.close();
+    };
+    MacNativeHost.prototype.ready = function () { };
+    return MacNativeHost;
+}());
+window['nativeHost'] = window['nativeHost'] || new MacNativeHost();
+//# sourceMappingURL=platform.js.map
