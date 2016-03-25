c8871d50222bb7ded12911c51344d0b9d5b454e3
Darren Reid <darren.j.reid@gmail.com>
2016-03-25 16:59:32 +1100

    Ref fix

--- src/ServiceStackVS/ProjectTemplates/ReactJSGap/ReactJSWinForms/AppHost.cs
+++ src/ServiceStackVS/ProjectTemplates/ReactJSGap/ReactJSWinForms/AppHost.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using System.Collections.Generic;
+using System.IO;
 using Funq;
 using ServiceStack;
 using Squirrel;
