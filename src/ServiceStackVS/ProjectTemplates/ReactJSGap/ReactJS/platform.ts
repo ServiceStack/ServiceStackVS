328fd181dad06781bdd03992a042043b97614f1b
Darren Reid <darren.j.reid@gmail.com>
2016-03-28 14:33:16 +1100

    Clean up web platform.ts

--- src/ServiceStackVS/ProjectTemplates/ReactJSGap/ReactJS/platform.ts
+++ src/ServiceStackVS/ProjectTemplates/ReactJSGap/ReactJS/platform.ts
@@ -1,7 +1,6 @@
-﻿// A '.tsx' file enables JSX support in the TypeScript compiler, 
-// for more information see the following page on the TypeScript wiki:
-// https://github.com/Microsoft/TypeScript/wiki/JSX
-/// <reference path='platform.d.ts'/>
+﻿interface Window {
+    nativeHost: any;
+}
 
 class WebNativeHost {
     showAbout() {
