1ae589ede0f24ffda6c53ad3e0bf6c13a0a0f082
Darren Reid <darren.j.reid@gmail.com>
2016-04-07 17:04:08 +1000

    Remove unused files

--- src/ServiceStackVS/ProjectTemplates/ReactJSGap/ReactJS/Scripts/_references.js
+++
@@ -1,6 +0,0 @@
-﻿//Add JS/CSS references here to enable intellisense from external packages.
-/// <reference path="~/bower_components/jquery/dist/jquery.js" />
-/// <reference path="~/bower_components/bootstrap/dist/js/bootstrap.js" />
-/// <reference path="~/node_modules/gulp/index.js"/>
-/// <reference path="~/bower_components/bootstrap/dist/css/bootstrap.css"/>
-/// <reference path="~/node_modules/grunt/lib/util/task.js"/>
