61e54839c056878d6148552133139f819382cf15
Darren Reid <darren.j.reid@gmail.com>
2016-04-21 11:01:50 +1000

    Fix Angular testing karma.conf

--- src/ServiceStackVS/ProjectTemplates/AngularSPA/AngularSPA/tests/karma.conf.js
+++ src/ServiceStackVS/ProjectTemplates/AngularSPA/AngularSPA/tests/karma.conf.js
@@ -12,9 +12,9 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      '../bower_components/angular/angular.js',
-      '../bower_components/angular-route/angular-route.js',
-      '../bower_components/angular-mocks/angular-mocks.js',
+      '../node_modules/angular/angular.js',
+      '../node_modules/angular-route/angular-route.js',
+      '../node_modules/angular-mocks/angular-mocks.js',
       '../js/**/*.js',
       'unit/**/*.js'
     ],
@@ -57,7 +57,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS'],
+    browsers: ['Chrome'],
 
 
     // Continuous Integration mode
