80ae978ba6eeaa4b88fb402d92c3d9e52b521d63
Darren Reid <darren.j.reid@gmail.com>
2016-05-19 16:51:33 +1000

    Update aurelia template with router built in

--- src/ServiceStackVS/ProjectTemplates/Aurelia/Host/src/app.ts
+++ src/ServiceStackVS/ProjectTemplates/Aurelia/Host/src/app.ts
@@ -1,4 +1,15 @@
 ﻿/// <reference path='../typings/browser.d.ts'/>
+import {Router} from 'aurelia-router';
+
 export class App {
-    message = '$safeprojectname$ Aurelia App';
+    router: Router;
+
+    configureRouter(config, router: Router) {
+        config.title = 'Aurelia';
+        config.map([
+            { route: ['', 'home'], name: 'home', moduleId: './hello', nav: true, title: 'Home' }
+        ]);
+
+        this.router = router;
+    }
 }
