e2bfe34b09c867eb9f70dd326c17e112690ddb62
Darren Reid <darren.j.reid@gmail.com>
2016-06-14 18:52:46 +1000

    Update TS templates typings to work with 1.0

--- src/ServiceStackVS/ProjectTemplates/TypeScriptReact/Host/src/hello.tsx
+++ src/ServiceStackVS/ProjectTemplates/TypeScriptReact/Host/src/hello.tsx
@@ -1,7 +1,7 @@
 ﻿// A '.tsx' file enables JSX support in the TypeScript compiler, 
 // for more information see the following page on the TypeScript wiki:
 // https://github.com/Microsoft/TypeScript/wiki/JSX
-/// <reference path='../typings/browser.d.ts'/>
+/// <reference path='../typings/index.d.ts'/>
 
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
