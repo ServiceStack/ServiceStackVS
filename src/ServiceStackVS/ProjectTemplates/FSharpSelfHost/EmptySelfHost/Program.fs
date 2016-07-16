922fa1acf094c188fc2036b830fe74a678557214
Demis Bellot <demis.bellot@gmail.com>
2016-07-16 00:18:10 -0400

    listen on 127.0.0.1 which seems to be most reliable

--- src/ServiceStackVS/ProjectTemplates/FSharpSelfHost/EmptySelfHost/Program.fs
+++ src/ServiceStackVS/ProjectTemplates/FSharpSelfHost/EmptySelfHost/Program.fs
@@ -9,7 +9,7 @@ module Main =
         let main argv = 
             let appHost = new AppHost()
             appHost.Init().Start("http://*:8088/") |> ignore
-            "ServiceStack SelfHost listening at http://localhost:8088 ".Print()
-            Process.Start("http://localhost:8088/") |> ignore
+            "ServiceStack SelfHost listening at http://127.0.0.1:8088".Print()
+            Process.Start("http://127.0.0.1:8088/") |> ignore
             Console.ReadLine() |> ignore
             0
