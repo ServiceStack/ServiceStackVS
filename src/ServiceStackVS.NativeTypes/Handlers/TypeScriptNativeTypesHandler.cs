7ce5ba7d77add49bbde67c33ac384657b60abaa7
Darren Reid <darren.j.reid@gmail.com>
2016-07-28 15:53:12 +1000

    Update Update ref for TS to support dtos.ts name

--- src/ServiceStackVS.NativeTypes/Handlers/TypeScriptNativeTypesHandler.cs
+++ src/ServiceStackVS.NativeTypes/Handlers/TypeScriptNativeTypesHandler.cs
@@ -1,10 +1,20 @@
-﻿namespace ServiceStackVS.NativeTypes.Handlers
+﻿using ServiceStack;
+
+namespace ServiceStackVS.NativeTypes.Handlers
 {
     public class TypeScriptNativeTypesHandler : BaseNativeTypesHandler, INativeTypesHandler
     {
         public override NativeTypesLanguage TypesLanguage { get { return NativeTypesLanguage.TypeScript; } }
         public override string CodeFileExtension { get { return ".dtos.d.ts"; } }
         public override string RelativeTypesUrl { get { return "types/typescript.d"; } }
+        public override bool IsHandledFileType(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName))
+            {
+                return false;
+            }
+            return fileName.EndsWithIgnoreCase(CodeFileExtension) || fileName == "dtos.d.ts";
+        }
     }
 
     public class TypeScriptConcreteNativeTypesHandler : BaseNativeTypesHandler, INativeTypesHandler
@@ -12,5 +22,13 @@
         public override NativeTypesLanguage TypesLanguage { get { return NativeTypesLanguage.TypeScriptConcrete; } }
         public override string CodeFileExtension { get { return ".dtos.ts"; } }
         public override string RelativeTypesUrl { get { return "types/typescript"; } }
+        public override bool IsHandledFileType(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName))
+            {
+                return false;
+            }
+            return fileName.EndsWithIgnoreCase(CodeFileExtension) || fileName == "dtos.ts";
+        }
     }
 }
