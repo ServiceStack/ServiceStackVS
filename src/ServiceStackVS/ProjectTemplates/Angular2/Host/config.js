f176638e5e5908ba913ad06786532f1af4d7e966
Darren Reid <darren.j.reid@gmail.com>
2016-08-24 20:16:02 +1000

    remove angular2 template.

--- src/ServiceStackVS/ProjectTemplates/Angular2/Host/config.js
+++
@@ -1,9 +0,0 @@
-System.config({
-  baseURL: "/",
-  defaultJSExtensions: true,
-  transpiler: false,
-  paths: {
-    "github:*": "jspm_packages/github/*",
-    "npm:*": "jspm_packages/npm/*"
-  }
-});
--- src/ServiceStackVS/ProjectTemplates/Angular2/Host/src/main.ts
+++
@@ -1,8 +0,0 @@
-import 'zone.js';
-import 'reflect-metadata';
-
-import { browserDynamicPlatform } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app.module';
-
-browserDynamicPlatform().bootstrapModule(AppModule);
