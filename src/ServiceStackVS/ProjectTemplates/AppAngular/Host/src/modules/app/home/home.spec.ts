f50f35fd769d91af13d6f522941963ba72a13297
Demis Bellot <demis.bellot@gmail.com>
2017-05-14 15:03:57 -0400

    Fix Angular test and add missing IISExpressAddress transform to .vstemplate

--- src/ServiceStackVS/ProjectTemplates/AppAngular/Host/src/modules/app/home/home.spec.ts
+++ src/ServiceStackVS/ProjectTemplates/AppAngular/Host/src/modules/app/home/home.spec.ts
@@ -1,7 +1,7 @@
 // This shows a different way of testing a component, see technology for a simpler one
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { ActivatedRoute, Data } from '@angular/router';
 import { TestBed } from '@angular/core/testing';
 
 import { HelloComponent } from './hello';
@@ -13,7 +13,17 @@ describe('Home Component', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [FormsModule],
-      declarations: [HelloComponent, HomeComponent, TestComponent]
+      declarations: [HelloComponent, HomeComponent, TestComponent],
+      providers: [{
+          provide: ActivatedRoute,
+          useValue: {
+              data: {
+                  subscribe: (fn: (value: Data) => void) => fn({
+                      title: 'Home', name: 'Angular4'
+                  })
+              }
+          }
+      }]
     });
     TestBed.overrideComponent(TestComponent, { set: { template: html }});
   });
@@ -21,7 +31,7 @@ describe('Home Component', () => {
   it ('Should render heading', () => {
     const fixture = TestBed.createComponent(TestComponent);
     fixture.detectChanges();
-    expect(fixture.nativeElement.querySelector('h4').textContent).toContain('Home');
+    expect(fixture.nativeElement.querySelector('h4').textContent).toContain('Welcome');
   });
 
 });
