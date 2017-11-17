bba5f0217af05cb9168d058aa9bb143e88e4aa4b
Demis Bellot <demis.bellot@gmail.com>
2017-11-17 04:40:34 -0500

    More copyright years

--- src/ServiceStackVS/ProjectTemplates/ASPNETRazor/Test/Properties/AssemblyInfo.cs
+++ src/ServiceStackVS/ProjectTemplates/ASPNETRazor/Test/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("$safeprojectname$")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
+[assembly: AssemblyCopyright("Copyright (c) 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
